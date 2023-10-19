f15d7f47bfa1c997346ea240694a6823f85e6aa0
Elias Rivera <eliasjohnrivera@gmail.com>
2023-10-19 14:00:32 -0700

    changed wording for front end

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -117,7 +117,7 @@ const SearchBooks = () => {
         <h2 className='pt-5 searchBodyStyling'>
           {searchedBooks.length
             ? `Viewing ${searchedBooks.length} results:`
-            : 'LOGIN OR SIGN UP TO SHOP!'}
+            : 'OPENING MINDS, ONE BOOK AT A TIME!'}
         </h2>
         <Row>
           {searchedBooks.map((book) => {
